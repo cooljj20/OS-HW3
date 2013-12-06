5274d93dd6329edfc5c9c951ce94622721e3a7c9
Jimmy Garcia <cooljj_20@sbcglobal.net>
2013-12-06 11:51:21 -0600

    Threading Problem

--- OS-HW3/OS-HW3/main.cpp
+++ OS-HW3/OS-HW3/main.cpp
@@ -7,12 +7,73 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+#include <cstring>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 int main(int argc, const char * argv[])
 {
+    int numberOfFlights;
+    int flightNumber;
+    int rows;
+    int seats;
+    int numberOfAgents;
+    
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ifstream myfile ("input.txt");
+    if (myfile.is_open())
+    {
+        string line;
+        //get numberOfFlights
+        getline(myfile,line);
+        stringstream(line) >> numberOfFlights;
+        
+        cout<<numberOfFlights<<endl;
+        
+        
+        //get flight seating information
+        istringstream iss;
+        stringstream ss;
+        
+        for(int i = 0; i<numberOfFlights; i++)
+        {
+            getline(myfile,line);
+            iss.str(line);
+            iss >> flightNumber;
+            iss >> rows;
+            iss >> seats;
+            
+            cout<<flightNumber<<" "<<rows<<" "  <<seats<<endl;
+        }
+        
+        //get numberOfAgents
+        getline(myfile,line);
+        stringstream(line) >> numberOfAgents;
+        cout<<numberOfAgents<<endl;
+
+        
+        for(int i = 1; i<numberOfAgents+1; i++)
+        {
+            getline(myfile,line);
+            while(line == "end.")
+            {
+                cout<<line<<endl;
+            }
+        }
+        
+    }
+    else cout << "Unable to open file\n";
+    myfile.close();
+    /* END READ FILE */
     return 0;
 }
 
